2ab434637d2a62fc08b5c97331977851bf51aeb3
Georgii Vlasov <honest.niceman@gmail.com>
2024-05-15 13:10:05 +0400

    Step 4. Adjust JPA model

--- flyway-and-spring-boot/blog/src/main/java/io/amplicode/blog/model/Post.java
+++ flyway-and-spring-boot/blog/src/main/java/io/amplicode/blog/model/Post.java
@@ -3,6 +3,8 @@ package io.amplicode.blog.model;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "posts")
@@ -20,6 +22,20 @@ public class Post extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
+
+    @ManyToMany
+    @JoinTable(name = "posts_comments",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "comments_id"))
+    private Set<Comment> comments = new LinkedHashSet<>();
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
 
     public String getTitle() {
         return title;
