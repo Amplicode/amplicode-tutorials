219106025d5cad4ea0dafbe9be39810468a2082d
Georgii Vlasov <honest.niceman@gmail.com>
2024-08-04 16:34:35 +0400

    3. Add proper Implementation for @EqualsAndHashCode

--- lombok-pitfalls/equals-and-hash-code/src/main/java/io/amplicode/equals_and_hash_code/entities/User.java
+++ lombok-pitfalls/equals-and-hash-code/src/main/java/io/amplicode/equals_and_hash_code/entities/User.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 //@EqualsAndHashCode
 @Getter
@@ -18,4 +21,30 @@ public class User {
 
     @Column(name = "username")
     private String username;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ? ((HibernateProxy) o).getHibernateLazyInitializer()
+                .getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                .getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                .getPersistentClass()
+                .hashCode()
+                : getClass().hashCode();
+    }
 }
