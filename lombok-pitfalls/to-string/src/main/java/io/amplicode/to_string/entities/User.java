b3f2e7e7d12869743c152b4018667c923b8b5600
Georgii Vlasov <honest.niceman@gmail.com>
2024-08-04 16:39:11 +0400

    4. @ToString leads to extra hits to database

--- lombok-pitfalls/to-string/src/main/java/io/amplicode/to_string/entities/User.java
+++ lombok-pitfalls/to-string/src/main/java/io/amplicode/to_string/entities/User.java
@@ -3,10 +3,12 @@ package io.amplicode.to_string.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+@ToString
 @Getter
 @Setter
 @Entity
